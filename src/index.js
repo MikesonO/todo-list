030adec5e2804e2dc6cb69a51845d4cd2438a020
mikeson <mikeson.jnr@gmail.com>
2022-10-09 14:31:03 +0100

    Set up Font Awesome

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+//CSS
 import "./styles.css";
-
-console.log("Working!");
+//Font Awesome
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
