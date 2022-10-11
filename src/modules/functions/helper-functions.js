a55a5d0d4b51a8cf63d8d7b6c1b936c0e708c8c8
mikeson <mikeson.jnr@gmail.com>
2022-10-11 03:25:56 +0100

    Add camelCasing function to helper-functions

--- src/modules/functions/helper-functions.js
+++ src/modules/functions/helper-functions.js
@@ -16,4 +16,10 @@ function enterKey(input, button){
       button.click();
 }})};
 
-export{inputCheck, resetInput, enterKey}
+function camelize(name) {
+  return name.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
+    return index === 0 ? word.toLowerCase() : word.toUpperCase();
+  }).replace(/\s+/g, '');
+}
+
+export{inputCheck, resetInput, enterKey, camelize}
