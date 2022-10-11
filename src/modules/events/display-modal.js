d351f52e9f83fbcfa01a096cfe14bb7ae50855ce
mikeson <mikeson.jnr@gmail.com>
2022-10-11 01:17:06 +0100

    Update modal display and animations

---
+++ src/modules/events/display-modal.js
@@ -0,0 +1,27 @@
+export const addProjectModal = (() => {
+  const modal = document.querySelector("[data-modal='new-project']");
+  const modalBox = document.querySelector(".modal");
+
+  const show = () =>{
+  modal.style.display = "block";
+  modalBox.classList.add("active");
+ }
+ 
+ const hide = () =>{
+  
+  console.log(modal)
+   window.setTimeout(()=>modal.style.display = "none", 400);
+   modalBox.classList.remove("active");
+ }
+
+
+ 
+ 
+ 
+ 
+ return{
+   show,
+   hide,
+ }
+ 
+ })();
