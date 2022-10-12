7f6fdda015c42badef7c2334bacab011b39cee0d
mikeson <mikeson.jnr@gmail.com>
2022-10-12 22:38:59 +0100

    Fix deleteBtn eventListener

--- src/modules/events/event-emitter.js
+++ src/modules/events/event-emitter.js
@@ -1,24 +1,29 @@
-
-import {addProjectModal} from "./display-modal";
-import {createProject} from "../functions/createProject";
+import { addProjectModal } from "./display-modal";
+import { createProject } from "../functions/createProject";
 import { enterKey } from "../functions/helper-functions";
 
 
-export function eventEmitter(){
+export function eventEmitter() {
   const addProjectButton = document.querySelector("#addProject-btn");
   addProjectButton.addEventListener("click", addProjectModal.show);
 
   const closeButton = document.querySelector(".close-btn");
   closeButton.addEventListener('click', addProjectModal.hide);
-  
-  const  createProjectButton = document.querySelector("[data-modal-btn='add']");
+
+  const createProjectButton = document.querySelector("[data-modal-btn='add']");
   const projectNameInput = document.querySelector("[data-modal-input='project']");
   enterKey(projectNameInput, createProjectButton); //Allows Enter keypress for input
-  createProjectButton.addEventListener("click", ()=>{
+  createProjectButton.addEventListener("click", () => {
     createProject.addProject();
     addProjectModal.hide();
   });
 
- 
-}
+  document.getElementById("projects").addEventListener("click", (e)=>{
+    if (e.target.classList.contains("delete-btn")){
+      createProject.deleteProject();
+    }
+  })
 
+  // svg-inline--fa fa-trash delete-btn
+
+}
--- src/modules/functions/createProject.js
+++ src/modules/functions/createProject.js
@@ -3,6 +3,8 @@ import { Project, projectList } from "../class/project";
 
 export const createProject = (() => {
 
+  const projects = document.getElementById("projects");
+
   const addProject = () =>{
     const projectNameInput = document.querySelector("[data-modal-input='project']");
     const projectName = projectNameInput.value;
@@ -12,27 +14,34 @@ export const createProject = (() => {
   }
 
   const appendProject = (name) =>{
-    const projects = document.getElementById("projects");
     projects.classList.add("show");
 
     const newProject = document.createElement("div"); //Creates New Project div
     newProject.setAttribute("class","left-rows");
+    newProject.setAttribute("data","project")
     newProject.setAttribute("id",`${camelize(name)}`);
 
     const newProjectName = document.createElement("p"); //Display Project Name
     newProjectName.textContent= `${name}`;
 
-    const deleteBtn = document.createElement("i"); //Creates Delete Button
-    let deleteBtnClasses = ["delete", "fa-solid", "fa-trash"];
-    deleteBtn.classList.add(...deleteBtnClasses);
+    const deleteBtn = document.createElement("button"); //Creates Delete Button
+    const deleteIcon = document.createElement("i");
+    let deleteIconClasses = ["delete-icon", "fa-solid", "fa-trash"];
+    deleteIcon.classList.add(...deleteIconClasses);
+    deleteBtn.setAttribute("class","delete-btn")
+    deleteBtn.appendChild(deleteIcon);
 
     newProject.append(newProjectName, deleteBtn);
 
-    const project = new Project(name, `${camelize(name)}`);
+    const project = new Project(name,`${camelize(name)}`);
     project.addProjects(project);
     console.log(projectList);
 
     projects.appendChild(newProject);
+  }
+
+  const deleteProject = ()=>{
+    console.log("Working!");
   }
 
   
@@ -41,7 +50,8 @@ export const createProject = (() => {
 
   return{
     addProject,
-    appendProject
+    appendProject,
+    deleteProject
   }
 
 })();
