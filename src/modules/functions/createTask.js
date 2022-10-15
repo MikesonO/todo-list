0bc610aef2393c2676569cadeac0893cd13137fa
mikeson <mikeson.jnr@gmail.com>
2022-10-15 16:13:22 +0100

    Add functionality to the tModal btn to create task

--- src/modules/functions/createTask.js
+++ src/modules/functions/createTask.js
@@ -1,4 +1,4 @@
-import { camelize, inputCheck,resetInput } from "./helper-functions";
+import { camelize, inputCheck, resetInput, capitalizeFirstLetter } from "./helper-functions";
 import { Project, projectList } from "../class/project";
 import { dateFormat } from "./dataFormat";
 
@@ -7,10 +7,12 @@ import { dateFormat } from "./dataFormat";
 
 export const createTask = (() => {
 
+  const tasks = document.querySelector("#task-list");
+
   const addTask = ()=>{
     //Gets Task Name Input
-    const taskNameInput = document.querySelector("[data-tModal-input='title']");
-    const taskName = taskNameInput.value;
+    const taskTitleInput = document.querySelector("[data-tModal-input='title']");
+    const taskTitle = taskTitleInput.value;
     //Gets Task Date Input
     const taskDateInput = document.querySelector("[data-tModal-input='date']");
     const taskDate = dateFormat.getDate(taskDateInput.value);
@@ -20,14 +22,52 @@ export const createTask = (() => {
     //Gets Task Description Input
     const taskDescriptionInput = document.querySelector("[data-tModal-input='description']");
     const taskdescription = taskDescriptionInput.value;
-    
-    console.log(taskName);
+
+    console.log(taskTitle);
     console.log(taskDate);
     console.log(taskPriority); 
     console.log(taskdescription); 
 
+    inputCheck(taskTitleInput);
+    // inputCheck(taskTitle, taskDate, taskPriority, taskdescription);
+    // resetInput(taskTitle, taskDate, taskPriority, taskdescription);
+    appendTask(taskTitle, taskDate, taskPriority);
   }
 
+  const appendTask = (taskTitle, taskDate, taskPriority) =>{
+
+    const newTask = document.createElement("div");
+    newTask.setAttribute("class","task");
+
+    const circle = document.createElement("div");
+    circle.setAttribute("class", "circle");
+
+    const title = document.createElement("div");
+    title.setAttribute("class","task-title");
+    title.textContent = taskTitle;
+
+    const date = document.createElement("div");
+    date.setAttribute("class","date");
+    date.textContent = taskDate;
+
+    const priority = document.createElement("div");
+    const priorityText = document.createElement("p");
+    priorityText.textContent = capitalizeFirstLetter(taskPriority)
+    const priorityAttributes = ["priority", `${taskPriority}`];
+    priority.classList.add(...priorityAttributes);
+    const fire = document.createElement("svg");
+    const fireAttributes = ["fa-solid", "fa-fire"];
+    fire.classList.add(...fireAttributes);
+    priority.append(fire, priorityText)
+   
+    
+    const ellipses = document.createElement("svg");
+    const ellipsesAttributes = ["fa-solid" ,"fa-ellipsis-vertical"];
+    ellipses.classList.add(...ellipsesAttributes);
+
+    newTask.append(circle, title, date, priority, ellipses);
+    tasks.appendChild(newTask);
+  }
 
 
 
