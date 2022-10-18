837384c3f2d4c8613936a2f1a9d43ca4221a0151
mikeson <mikeson.jnr@gmail.com>
2022-10-18 23:42:21 +0100

    Fix displayAllTasks function duplicating inbox

--- src/modules/functions/defaultProjects.js
+++ src/modules/functions/defaultProjects.js
@@ -1,20 +1,20 @@
-import { project as getProjects} from "./projects";
-import { task as getTasks} from "./tasks";
+import { task as getTasks } from "./tasks";
 import { projectList } from "../classes/projectClass";
 
 export const defaultProjects = (() => {
 
 
-  const displayAllTasks = () =>{
-    getProjects.clearProjectTasks();
-    projectList.forEach((project) =>{
-      project.tasks.forEach((task) => {
-        getTasks.appendTask(task.project, task.title, task.date, task.priority);
-      });
-  });
+  const displayAllTasks = () => {
+    projectList.forEach((project) => {
+      if (project.id !== "inbox") {
+        project.tasks.forEach((task) => {
+          getTasks.appendTask(task.project, task.title, task.date, task.priority);
+        });
+      }
+    });
   }
 
-  
+
 
 
 
